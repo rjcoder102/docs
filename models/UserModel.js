7d8da8792ca20feb3b9f9946437d8289fd66c8d3
hbfbdb599-hash <hbfbdb599@gmail.com>
2026-06-27 15:10:12 +0530

    Update UserModel.js

--- models/UserModel.js
+++ models/UserModel.js
@@ -12,7 +12,7 @@ const userSchema = new mongoose.Schema(
             type: String, 
             required: true 
         },
-         by: { 
+        by: { 
         type: String, 
         default:"zapcore"
         },
